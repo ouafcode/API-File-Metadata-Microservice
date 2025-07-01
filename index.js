ee60f0f6bd9d4c361b68874b9e7ac30164553f33
ouafcode <ouafaesaim@gmail.com>
2025-07-01 17:09:50 +0100

    update

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config();
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.post('/api/fileanalyse', upload.single('upfile'), (req,res)=>{
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
   const file = req.file;
-  res.json({
-    name: file.originalname, type: file.mimetype, size: file.size
-  })
-})
 
+  if (!file) {
+    return res.status(400).json({ error: "No file uploaded" });
+  }
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size: file.size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
